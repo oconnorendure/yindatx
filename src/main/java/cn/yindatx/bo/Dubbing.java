2858cec618b2881e0136dd0e0bb476522faba012
oconnorendure <oconnorendure@gmail.com>
2015-11-24 15:10:46 +0800

    mp3 file

---
+++ src/main/java/cn/yindatx/bo/Dubbing.java
@@ -0,0 +1,10 @@
+package cn.yindatx.bo;
+import com.jfinal.plugin.activerecord.Model;
+
+public class Dubbing extends Model<Dubbing>{
+
+	private static final long serialVersionUID = 2200796242392568256L;
+
+	public static final Dubbing dao = new Dubbing();
+	
+}
---
+++ src/main/java/cn/yindatx/bo/Music.java
@@ -0,0 +1,9 @@
+package cn.yindatx.bo;
+
+import com.jfinal.plugin.activerecord.Model;
+
+public class Music extends Model<Music>{
+	private static final long serialVersionUID = 8203270764860283524L;
+
+	public static final Music dao = new Music();
+}
